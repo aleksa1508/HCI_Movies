64866f24bcf0a9ff80a97a195100682cc704a915
aleksa1508 <aleksaarsenic15.08@gmail.com>
2025-07-15 17:37:19 +0200

    prvi commit

---
+++ Filmovi/Filmovi/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Filmovi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
